624ba1e96797c45c30e4d36c748da296c5675798
birukzerihun <birukz516@gmail.com>
2024-06-08 23:43:45 +0300

    A Better File Structure

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'This route is not done yet',
+  });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'This route is not done yet',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'This route is not done yet',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'This route is not done yet',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'fail',
+    message: 'This route is not done yet',
+  });
+};

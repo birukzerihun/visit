9766c96c6ac7ad63d5502c71f6d1737d7f45bb78
birukzerihun <birukz516@gmail.com>
2024-06-09 13:31:30 +0300

    Environment Variables

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const app = express();
 
 //1) MIDDLEWARE
 //  ./ === ${__dirname}
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
-app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {

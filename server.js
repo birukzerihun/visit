a3e0f025009d74fca5e8740985bd6bc7ef06197c
birukzerihun <birukz516@gmail.com>
2024-06-11 23:54:04 +0300

    Refactoring for MVC

--- server.js
+++ server.js
@@ -16,36 +16,6 @@ mongoose
     console.log('DB connected successfully');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const newTour = new Tour({
-  name: 'The Forest Ranger',
-  rating: 4.7,
-  price: 2997
-});
-
-newTour
-  .save()
-  .then(doc => console.log(doc))
-  .catch(err => console.log(err));
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);

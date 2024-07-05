1e6cb0e5edac4d20a33f50a49029bf30fe896cae
birukzerihun <birukz516@gmail.com>
2024-07-05 15:44:24 +0300

    Logging In

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,14 @@
 const jwt = require('jsonwebtoken');
 
+const AppError = require('./../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
+
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
 
 exports.signUp = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -11,12 +18,31 @@ exports.signUp = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
-  });
+  const token = signToken(newUser._id);
+
   res.status(201).json({
     status: 'success',
     token,
     data: { user: newUser }
+  });
+});
+
+exports.logIn = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  //1) check if email and password exists
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  //2) check user exists and password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Invalid email or password', 401));
+  }
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token
   });
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,13 @@
-exports.getAllUsers = (req, res) => {
+const User = require('./../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
   res.status(500).json({
-    status: 'fail',
-    message: 'This route is not done yet'
+    status: 'success',
+    users: users
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
